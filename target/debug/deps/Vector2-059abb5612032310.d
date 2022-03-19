f74c6d6193d3e215c53dca5200dcffa27528361b
Cubidev3 <100206395+Cubidev3@users.noreply.github.com>
2022-03-19 11:30:28 -0300

    Uploaded first files

---
+++ target/debug/deps/Vector2-059abb5612032310.d
@@ -0,0 +1,5 @@
+/home/cubi/Documentos/Rust/Vector2/target/debug/deps/Vector2-059abb5612032310.rmeta: src/lib.rs
+
+/home/cubi/Documentos/Rust/Vector2/target/debug/deps/Vector2-059abb5612032310.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/vector2-0f132338a4508679.d
@@ -0,0 +1,5 @@
+/home/cubi/Documentos/Rust/Vector2/target/debug/deps/vector2-0f132338a4508679.rmeta: src/lib.rs
+
+/home/cubi/Documentos/Rust/Vector2/target/debug/deps/vector2-0f132338a4508679.d: src/lib.rs
+
+src/lib.rs:
