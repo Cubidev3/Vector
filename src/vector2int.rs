054a5ed4802b7dd03672db36036e2be78554fb0f
Cubidev3 <100206395+Cubidev3@users.noreply.github.com>
2022-03-23 14:17:01 -0300

    Derives, ONE enum and more
    for Vector2Int and Vector3Int {
        - Added Derives: Copy, Clone, PartialEq and Eq;
        - Added Vector2Int::ONE and Vector3Int::ONE;
        - Added opposite();
        - Added Operations
    }

--- src/vector2int.rs
+++ src/vector2int.rs
@@ -1,5 +1,7 @@
+use super::vector3int::Vector3Int;
 use std::ops;
 
+#[derive(Copy, Clone, PartialEq, Eq)]
 pub struct Vector2Int {
     pub x: i32,
     pub y: i32,
@@ -7,6 +9,7 @@ pub struct Vector2Int {
 
 impl Vector2Int {
     pub const ZERO: Vector2Int = Vector2Int { x: 0, y: 0 };
+    pub const ONE: Vector2Int = Vector2Int { x: 1, y: 1 };
     pub const UP: Vector2Int = Vector2Int { x: 0, y: 1 };
     pub const DOWN: Vector2Int = Vector2Int { x: 0, y: -1 };
     pub const LEFT: Vector2Int = Vector2Int { x: -1, y: 0 };
@@ -47,12 +50,65 @@ impl Vector2Int {
             y: self.y.abs(),
         }
     }
+
+    pub fn oposite(&self) -> Vector2Int {
+        Vector2Int {
+            x: self.x * -1,
+            y: self.y * -1,
+        }
+    }
+}
+
+impl ops::Add for Vector2Int {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Vector2Int {
+        Vector2Int {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl ops::Add<Vector3Int> for Vector2Int {
+    type Output = Vector3Int;
+
+    fn add(self, rhs: Vector3Int) -> Vector3Int {
+        Vector3Int {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: rhs.z,
+        }
+    }
 }
 
 impl ops::AddAssign<Vector2Int> for Vector2Int {
     fn add_assign(&mut self, rhs: Vector2Int) {
         self.x += rhs.x;
         self.y += rhs.y;
+    }
+}
+
+impl ops::Sub for Vector2Int {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Vector2Int {
+        Vector2Int {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl ops::Sub<Vector3Int> for Vector2Int {
+    type Output = Vector3Int;
+
+    fn sub(self, rhs: Vector3Int) -> Vector3Int {
+        Vector3Int {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: 0 - rhs.z,
+        }
     }
 }
 
@@ -63,6 +119,51 @@ impl ops::SubAssign<Vector2Int> for Vector2Int {
     }
 }
 
+impl ops::Mul for Vector2Int {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Vector2Int {
+        Vector2Int {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+        }
+    }
+}
+
+impl ops::Mul<Vector2Int> for i32 {
+    type Output = Vector2Int;
+
+    fn mul(self, rhs: Vector2Int) -> Vector2Int {
+        Vector2Int {
+            x: rhs.x * self,
+            y: rhs.y * self,
+        }
+    }
+}
+
+impl ops::Mul<i32> for Vector2Int {
+    type Output = Self;
+
+    fn mul(self, rhs: i32) -> Vector2Int {
+        Vector2Int {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
+impl ops::Mul<Vector3Int> for Vector2Int {
+    type Output = Vector3Int;
+
+    fn mul(self, rhs: Vector3Int) -> Vector3Int {
+        Vector3Int {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: 0,
+        }
+    }
+}
+
 impl ops::MulAssign<i32> for Vector2Int {
     fn mul_assign(&mut self, rhs: i32) {
         self.x *= rhs;
@@ -70,9 +171,45 @@ impl ops::MulAssign<i32> for Vector2Int {
     }
 }
 
+impl ops::MulAssign<Vector2Int> for Vector2Int {
+    fn mul_assign(&mut self, rhs: Vector2Int) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+    }
+}
+
+impl ops::Div<i32> for Vector2Int {
+    type Output = Self;
+
+    fn div(self, rhs: i32) -> Vector2Int {
+        Vector2Int {
+            x: self.x / rhs,
+            y: self.y / rhs,
+        }
+    }
+}
+
+impl ops::Div for Vector2Int {
+    type Output = Self;
+
+    fn div(self, rhs: Vector2Int) -> Vector2Int {
+        Vector2Int {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+        }
+    }
+}
+
 impl ops::DivAssign<i32> for Vector2Int {
     fn div_assign(&mut self, rhs: i32) {
         self.x /= rhs;
         self.y /= rhs;
+    }
+}
+
+impl ops::DivAssign<Vector2Int> for Vector2Int {
+    fn div_assign(&mut self, rhs: Vector2Int) {
+        self.x /= rhs.x;
+        self.y /= rhs.y;
     }
 }
--- src/vector3int.rs
+++ src/vector3int.rs
@@ -1,6 +1,7 @@
 use super::vector2int::Vector2Int;
 use std::ops;
 
+#[derive(Copy, Clone, PartialEq, Eq)]
 pub struct Vector3Int {
     pub x: i32,
     pub y: i32,
@@ -9,6 +10,7 @@ pub struct Vector3Int {
 
 impl Vector3Int {
     pub const ZERO: Vector3Int = Vector3Int { x: 0, y: 0, z: 0 };
+    pub const ONE: Vector3Int = Vector3Int { x: 1, y: 1, z: 1 };
     pub const UP: Vector3Int = Vector3Int { x: 0, y: 1, z: 0 };
     pub const DOWN: Vector3Int = Vector3Int { x: 0, y: -1, z: 0 };
     pub const LEFT: Vector3Int = Vector3Int { x: -1, y: 0, z: 0 };
@@ -59,6 +61,38 @@ impl Vector3Int {
             z: self.z.abs(),
         }
     }
+
+    pub fn oposite(&self) -> Vector3Int {
+        Vector3Int {
+            x: self.x * -1,
+            y: self.y * -1,
+            z: self.z * -1,
+        }
+    }
+}
+
+impl ops::Add for Vector3Int {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Vector3Int {
+        Vector3Int {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl ops::Add<Vector2Int> for Vector3Int {
+    type Output = Self;
+
+    fn add(self, rhs: Vector2Int) -> Vector3Int {
+        Vector3Int {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z,
+        }
+    }
 }
 
 impl ops::AddAssign<Vector3Int> for Vector3Int {
@@ -69,10 +103,34 @@ impl ops::AddAssign<Vector3Int> for Vector3Int {
     }
 }
 
-impl  ops::AddAssign<Vector2Int> for Vector3Int {
+impl ops::AddAssign<Vector2Int> for Vector3Int {
     fn add_assign(&mut self, rhs: Vector2Int) {
         self.x += rhs.x;
         self.y += rhs.y;
+    }
+}
+
+impl ops::Sub for Vector3Int {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Vector3Int {
+        Vector3Int {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl ops::Sub<Vector2Int> for Vector3Int {
+    type Output = Self;
+
+    fn sub(self, rhs: Vector2Int) -> Vector3Int {
+        Vector3Int {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z,
+        }
     }
 }
 
@@ -91,6 +149,54 @@ impl ops::SubAssign<Vector2Int> for Vector3Int {
     }
 }
 
+impl ops::Mul for Vector3Int {
+    type Output = Self;
+
+    fn mul(self, rhs: Vector3Int) -> Vector3Int {
+        Vector3Int {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        }
+    }
+}
+
+impl ops::Mul<Vector2Int> for Vector3Int {
+    type Output = Self;
+
+    fn mul(self, rhs: Vector2Int) -> Vector3Int {
+        Vector3Int {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: 0,
+        }
+    }
+}
+
+impl ops::Mul<i32> for Vector3Int {
+    type Output = Self;
+
+    fn mul(self, rhs: i32) -> Vector3Int {
+        Vector3Int {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl ops::Mul<Vector3Int> for i32 {
+    type Output = Vector3Int;
+
+    fn mul(self, rhs: Vector3Int) -> Vector3Int {
+        Vector3Int {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        }
+    }
+}
+
 impl ops::MulAssign<i32> for Vector3Int {
     fn mul_assign(&mut self, rhs: i32) {
         self.x *= rhs;
@@ -99,10 +205,58 @@ impl ops::MulAssign<i32> for Vector3Int {
     }
 }
 
+impl ops::MulAssign<Vector3Int> for Vector3Int {
+    fn mul_assign(&mut self, rhs: Vector3Int) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+        self.z *= rhs.z;
+    }
+}
+
+impl ops::MulAssign<Vector2Int> for Vector3Int {
+    fn mul_assign(&mut self, rhs: Vector2Int) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+        self.z = 0;
+    }
+}
+
+impl ops::Div<Vector3Int> for Vector3Int {
+    type Output = Self;
+
+    fn div(self, rhs: Vector3Int) -> Vector3Int {
+        Vector3Int {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+            z: self.z / rhs.z,
+        }
+    }
+}
+
+impl ops::Div<i32> for Vector3Int {
+    type Output = Self;
+
+    fn div(self, rhs: i32) -> Vector3Int {
+        Vector3Int {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
+    }
+}
+
 impl ops::DivAssign<i32> for Vector3Int {
     fn div_assign(&mut self, rhs: i32) {
         self.x /= rhs;
         self.y /= rhs;
         self.z /= rhs;
+    }
+}
+
+impl ops::DivAssign<Vector3Int> for Vector3Int {
+    fn div_assign(&mut self, rhs: Vector3Int) {
+        self.x /= rhs.x;
+        self.y /= rhs.y;
+        self.z /= rhs.z;
     }
 }
